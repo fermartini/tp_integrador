921ae5d98d3cfb3c81fbb083794cd6f4497e4858
Mtreleani <88211502+Mtreleani@users.noreply.github.com>
2024-10-03 20:27:13 -0300

    Update HistoryActivity.kt
    cambié el vinculo a activity_history.xml, en mi compu no me aparecía y tuve que usar como alternativa activity_historyresults.xml que ahora ha quedado descartado y no subiré a este repo

--- app/src/main/java/ar/com/tp_integrador/HistoryActivity.kt
+++ app/src/main/java/ar/com/tp_integrador/HistoryActivity.kt
@@ -14,7 +14,7 @@ import android.widget.ListView
 class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_historyresults)
+        setContentView(R.layout.activity_history)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
