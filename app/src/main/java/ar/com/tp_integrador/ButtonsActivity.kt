ad2d43269aded3a2eeaafb6310636bc2f974d271
LeilaGiselleDeLuca <leila.giselle.de.luca@gmail.com>
2024-10-04 20:48:03 -0300

    clean code

--- app/src/main/java/ar/com/tp_integrador/ButtonsActivity.kt
+++ app/src/main/java/ar/com/tp_integrador/ButtonsActivity.kt
@@ -8,8 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class ButtonsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ar/com/tp_integrador/LoginActivity.kt
+++ app/src/main/java/ar/com/tp_integrador/LoginActivity.kt
@@ -10,8 +10,6 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
